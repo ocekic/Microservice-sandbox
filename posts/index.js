3023d3647bd88d8456edab1ccdce5d04ef319345
Osman <96057877+ocekic@users.noreply.github.com>
2023-07-10 11:55:29 +0200

    Starting the development

---
+++ posts/index.js
@@ -0,0 +1,16 @@
+// @ts-nocheck
+const express = require('express');
+
+const app = express();
+
+app.get('/posts', (req,res)=>{
+
+});
+
+app.post('/posts', (req,res)=>{
+
+});
+
+app.listen(4000,()=>{
+    console.log('Listening on port 4000');
+});
