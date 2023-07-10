caf3b0d8f713dab5b9000b310e8f16f3742d72a2
Osman <96057877+ocekic@users.noreply.github.com>
2023-07-10 19:20:45 +0200

    The controller posts and the request get/posts are working

--- posts/index.js
+++ posts/index.js
@@ -1,13 +1,33 @@
 // @ts-nocheck
 const express = require('express');
+const {randomBytes} = require('crypto');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+
 
 app.get('/posts', (req,res)=>{
-
+    res.send(posts);
 });
 
 app.post('/posts', (req,res)=>{
+
+    // Generate a random id
+    const id = randomBytes(4).toString('hex');
+
+    // Get the title from the request body
+    const {title} = req.body;
+
+    // Add the post to the posts object
+    posts[id] = {
+        id,
+        title,
+    };
+
+    res.status(201).send(posts[id]);
 
 });
 
